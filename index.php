e5a61eeb3b1a7a75fce1da973379e0d60876eac6
emektep <156540373+emektep@users.noreply.github.com>
2024-01-13 21:57:35 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Ipsum facere sit nam quos voluptas, odio possimus quidem ab voluptatum dignissimos ratione deleniti. Sapiente atque dolorem recusandae illo maxime quod, eos!
+</body>
+</html>
